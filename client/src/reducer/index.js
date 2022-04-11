c2f83f3944ac913c5793cf9955698f93752fa8c6
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-04-10 21:26:13 -0300

    improved filters

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -6,6 +6,7 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, { type, payload }) {
+  // let dogsStorage = state.dogsStorage
   switch (type) {
     case "GET_DOGS":
       return {
@@ -46,23 +47,29 @@ export default function reducer(state = initialState, { type, payload }) {
         dogs: alphabetArr,
       };
     case "ORDER_BY_WEIGHT":
+      let prom = (w)=>{ 
+        let p = w.split('-')
+        let min = parseInt(p[0])
+        let max = parseInt(p[1])
+        return Math.round((min+max)/2)
+      }
       let arr =
         payload === "high"
           ? state.dogs.sort(function (a, b) {
-              if (parseInt(a.weight) < parseInt(b.weight)) {
+              if (prom(a.weight) < prom(b.weight)) {
                 return 1;
               }
-              if (parseInt(a.weight) > parseInt(b.weight)) {
+              if (prom(a.weight) > prom(b.weight)) {
                 return -1;
               } else {
                 return 0;
               }
             })
           : state.dogs.sort(function (a, b) {
-              if (parseInt(a.weight) > parseInt(b.weight)) {
+              if (prom(a.weight) > prom(b.weight)) {
                 return 1;
               }
-              if (parseInt(a.weight) < parseInt(b.weight)) {
+              if (prom(a.weight) < prom(b.weight)) {
                 return -1;
               } else {
                 return 0;
@@ -85,14 +92,25 @@ export default function reducer(state = initialState, { type, payload }) {
         temperaments: payload,
       };
       case "FILTER_TEMPERAMENTS":
-      const dogsStorage = state.dogsStorage;
-      const filteredTemperaments =
+      let dogsStorage = state.dogsStorage;
+      let filteredTemperaments 
         payload === "All"
-          ? dogsStorage
-          : dogsStorage.filter((e) => e.temperament?.includes(payload));
+          ? filteredTemperaments = dogsStorage
+          : filteredTemperaments = dogsStorage.filter((e) => e.temperament?.includes(payload)).concat(dogsStorage.filter(e=> e.temperaments?.map(e=> e.name).includes(payload)));
       return {
         ...state,
         dogs: filteredTemperaments,
+      };
+      case "FILTER_ORIGIN":
+      let dogsStorageAux = state.dogsStorage;
+      let filteredTemperamentsAux = dogsStorageAux
+        if(payload === "Api"){
+          filteredTemperamentsAux = dogsStorageAux.filter((e) => e.id.toString().length < 4)}
+        if(payload === "Db"){
+          filteredTemperamentsAux = dogsStorageAux.filter((e) => e.id.toString().length > 4)}
+      return {
+        ...state,
+        dogs: filteredTemperamentsAux,
       };
       case 'POST_DOG':
         return{
